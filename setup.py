9f00c548eb2e8f73612a0dd531edd3336f325e6e
Pietro Stradiotti <pietro.stradiotti@tuwien.ac.at>
2021-11-05 16:07:11 +0100

    changes setuptools requirement

--- setup.py
+++ setup.py
@@ -13,9 +13,9 @@ from pkg_resources import VersionConflict, require
 from setuptools import setup
 
 try:
-    require('setuptools>=38.3')
+    require('setuptools>=58.0')
 except VersionConflict:
-    print("Error: __version of setuptools is too old (<38.3)!")
+    print("Error: __version of setuptools is too old (<58.0)!")
     sys.exit(1)
 
 
