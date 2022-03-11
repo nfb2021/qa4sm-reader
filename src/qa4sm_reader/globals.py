58577b320c1fecdde77736f1bae6f05a8b73e987
Wolfgang Preimesberger <wolfgang.preimesberger@geo.tuwien.ac.at>
2022-03-11 15:27:33 +0100

    Update dataset names

--- src/qa4sm_reader/globals.py
+++ src/qa4sm_reader/globals.py
@@ -218,16 +218,19 @@ def get_metric_units(dataset, raise_error=False):
     # function to get m.u. with possibility to raise error
     _metric_units = {  # from /qa4sm/validator/validation/graphics.py
         'ISMN': 'm³/m³',
-        'C3S': 'm³/m³',
+        'C3S': 'm³/m³',  # old name
+        'C3S_combined': 'm³/m³',
         'GLDAS': 'm³/m³',
         'ASCAT': '% saturation',
-        'SMAP': 'm³/m³',
+        'SMAP': 'm³/m³',   # old name
+        'SMAP_L3': 'm³/m³',
         'ERA5': 'm³/m³',
         'ERA5_LAND': 'm³/m³',
         'ESA_CCI_SM_active': '% saturation',
         'ESA_CCI_SM_combined': 'm³/m³',
         'ESA_CCI_SM_passive': 'm³/m³',
-        'SMOS': 'm³/m³',
+        'SMOS': 'm³/m³',   # old name
+        'SMOS_IC': 'm³/m³',
         'CGLS_CSAR_SSM1km': '% saturation',
         'CGLS_SCATSAR_SWI1km': '% saturation',
         'SMOS_L3': 'm³/m³',
