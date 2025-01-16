bd627d41e6e6652f0ca96997f7acb8ef1645d444
nfb2021 <nicolas.bader@geo.tuwien.ac.at>
2025-01-16 13:26:05 +0100

    compression enabled on windows OS but test skipped

--- src/qa4sm_reader/netcdf_transcription.py
+++ src/qa4sm_reader/netcdf_transcription.py
@@ -573,9 +573,9 @@ class Pytesmo2Qa4smResultsTranscriber:
         """
 
         # compression does not work sometimes on Windows. Thus, it is disabled
-        if sys.platform.startswith("win"):
-            # return True so that the corresponding test still passes
-            return True
+        # if sys.platform.startswith("win"):
+        #     # return True so that the corresponding test still passes
+        #     return True
 
         if compression in IMPLEMENTED_COMPRESSIONS and complevel in ALLOWED_COMPRESSION_LEVELS:
 
--- tests/test_netcdf_transcription.py
+++ tests/test_netcdf_transcription.py
@@ -8,6 +8,7 @@ from typing import Union, Optional, Tuple, List
 import logging
 import numpy as np
 import tempfile
+import sys
 
 from qa4sm_reader.netcdf_transcription import Pytesmo2Qa4smResultsTranscriber, TemporalSubWindowMismatchError
 from qa4sm_reader.intra_annual_temp_windows import TemporalSubWindowsCreator, NewSubWindow, InvalidTemporalSubWindowError, TemporalSubWindowsFactory
@@ -386,19 +387,20 @@ def test_ncfile_compression(TEST_DATA_DIR, test_file: Optional[Path] = None):
                                            keep_pytesmo_ncfile=False,
                                            write_outfile=True)
 
-    # only zlib compression is implemented so far, with compression levels 0-9
-    with pytest.raises(NotImplementedError):
-        transcriber.compress(transcriber.output_file_name, 'not_implemented',
-                             0)
-        transcriber.compress(transcriber.output_file_name, 'zlib', -1)
-        transcriber.compress(transcriber.output_file_name, 'not_implemented',
-                             -1)
+    if not sys.platform.startswith("win"):
+        # only zlib compression is implemented so far, with compression levels 0-9
+        with pytest.raises(NotImplementedError):
+            transcriber.compress(transcriber.output_file_name, 'not_implemented',
+                                0)
+            transcriber.compress(transcriber.output_file_name, 'zlib', -1)
+            transcriber.compress(transcriber.output_file_name, 'not_implemented',
+                                -1)
 
-    # test the case of a non-existing file
-    assert not transcriber.compress('non_existing_file.nc', 'zlib', 0)
+        # test the case of a non-existing file
+        assert not transcriber.compress('non_existing_file.nc', 'zlib', 0)
 
-    # test successful compression with zlib and compression level 9
-    assert transcriber.compress(transcriber.output_file_name, 'zlib', 9)
+        # test successful compression with zlib and compression level 9
+        assert transcriber.compress(transcriber.output_file_name, 'zlib', 9)
 
     # test successful compression with defaults
     assert transcriber.compress(transcriber.output_file_name)
